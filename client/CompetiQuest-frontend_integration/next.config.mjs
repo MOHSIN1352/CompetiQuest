c3bfc636f9473f5f5b10fc11b9262f4580973dc6
Mohsin Pathan <106861993+MOHSIN1352@users.noreply.github.com>
2025-11-29 19:45:05 +0530

    Docker standalone changes

--- client/CompetiQuest-frontend_integration/next.config.mjs
+++ client/CompetiQuest-frontend_integration/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // CRITICAL: Enable the standalone output for Docker
+  output: 'standalone', 
+};
 
 export default nextConfig;
